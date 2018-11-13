76fd3317ea4e21d34ebda3a0e2e953c495e1619e
APCS3rd <APCS3rd@MHS-Dreyer-01>
2018-11-13 11:32:56 -0800

    Figure out how to remove an element from an Array

--- Arrays/src/ArraysLab3.java
+++ Arrays/src/ArraysLab3.java
@@ -3,21 +3,26 @@ import java.util.Arrays;
 public class ArraysLab3 {
 
 	public static void main(String[] args) {
+	//declare array values
+	int[] Arr  = {2, 4, 6, 8};
 	int[] Arr1 = {3, 6, 9, 12};
 	int[] Arr2 = {1, 2, 3, 4};
+	//methods
 	System.out.println("Sum of arrays");
 	Sum(Arr1, Arr2);
-	int[] Arr = {2, 4, 6, 8};
-	int Add = 1;
 	System.out.println("Array indexes incremented by 1");
-	append(Arr, Add);
+	int num = 1;
+	append(Arr, num);
+	System.out.println("Array looks for a value and removes it from the array");
+	int idx = 6;
+	remove(Arr,idx);
 	//place methods here!!!
 	}
 	public static int[] Sum(int[] Arr1, int[] Arr2) {
 		for (int i = 0; i<Arr1.length; i++) {
 			System.out.println(Arr1[i] + Arr2[i]);		
-			}
-			return Arr2;
+		}
+		return Arr2;
 	}
 	public static int[] append(int[] Arr, int num) {
 		for (int i = 0; i<Arr.length; i++) {
@@ -25,5 +30,14 @@ public class ArraysLab3 {
 		}
 		return Arr;
 	}
-	public static i
+	public static int[] remove(int[] Arr, int idx) {
+		for (int i = 0; i < Arr.length - 1 ; i++) {
+			Arr[i] = Arr[i+1];
+		}
+	}
 }
+
+
+
+
+
