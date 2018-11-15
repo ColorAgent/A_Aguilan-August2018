66bf955ea6613ef28bd2abcf08c57e16e018c1be
Alex Aguilan <Alex_Aguilan@users.noreply.github.com>
2018-11-14 23:54:30 -0800

    Figure out rotateright and fix Sumeven(indices idk)

--- Arrays/src/ArraysLab3.java
+++ Arrays/src/ArraysLab3.java
@@ -4,39 +4,68 @@ public class ArraysLab3 {
 
 	public static void main(String[] args) {
 	//declare array values
-	int[] Arr  = {2, 4, 6, 8};
+	//            0  1  2  3
+	int[] arr  = {2, 4, 6, 8};
 	int[] Arr1 = {3, 6, 9, 12};
 	int[] Arr2 = {1, 2, 3, 4};
-	//methods
-	System.out.println("Sum of arrays");
-	Sum(Arr1, Arr2);
-	System.out.println("Array indexes incremented by 1");
 	int num = 1;
-	append(Arr, num);
-	System.out.println("Array looks for a value and removes it from the array");
-	int idx = 6;
-	remove(Arr,idx);
-	//place methods here!!!
+	//methods
+	System.out.println("Arr: " + Arrays.toString(arr));
+	System.out.println("Arr1: " + Arrays.toString(Arr1));
+	System.out.println("Arr2: " + Arrays.toString(Arr2));
+	System.out.println("Sum: " + Arrays.toString(Sum(Arr1, Arr2)));
+	System.out.println("Append: " + Arrays.toString(append(arr, num)));
+	System.out.println("Remove: " + Arrays.toString(remove(arr, 2)));
+	System.out.println("SumEven: " + (sumEven(arr)));
+	System.out.println("rotateRight: " + (rotateRight(arr)));
+	
 	}
+	//Arr1 indexes plus Arr2 indexes
 	public static int[] Sum(int[] Arr1, int[] Arr2) {
 		for (int i = 0; i<Arr1.length; i++) {
-			System.out.println(Arr1[i] + Arr2[i]);		
+			Arr1[i] = Arr1[i] + Arr2[i];		
 		}
-		return Arr2;
+		return Arr1;
 	}
+	//Arr indexes incremented by num
 	public static int[] append(int[] Arr, int num) {
 		for (int i = 0; i<Arr.length; i++) {
-			System.out.println(Arr[i] + num);		
+			Arr[i] = Arr[i] + num;
 		}
 		return Arr;
 	}
-	public static int[] remove(int[] Arr, int idx) {
-		for (int i = 0; i < Arr.length - 1 ; i++) {
-			Arr[i] = Arr[i+1];
+	// Array looks for a value and removes it from the array
+	public static int[] remove(int[] arr, int idx) {
+		//create a new set of arrays with the last index removed
+		int[] NewArray = new int[arr.length-1];
+		int index = 0;
+		for(int i = 0; i < arr.length; i++) {
+			if(i != idx) {
+				NewArray[index] = arr[i];
+				index++;
+			}
+		}
+		return NewArray;
+	}
+	// Accepts an array of ints and returns an integer thats the sum of all the even index numbers
+	public static int sumEven(int[] arr) {
+		int sum = 0;
+		for(int i = 0; i<arr.length; i++) {
+			if(i % 2 == 0) {
+				sum = sum + arr[i];
+			}
+		}
+		return sum;
+	}
+	// Rotates all elements of an array to the right by 1
+	public static void rotateRight (int[] arr) {
+		int[] NewArray = new int[arr.length-1];
+		for(int i = 0; i < arr.length; i++) {
+			NewArray[i] = arr.length;
 		}
 	}
+	return arr;
 }
-
 
 
 
