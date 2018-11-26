efa36f04b97b5d017997f43df5e4626a9767153b
Alex Aguilan <Alex_Aguilan@users.noreply.github.com>
2018-11-25 22:34:20 -0800

    idk

--- SplitString/src/split.java
+++ SplitString/src/split.java
@@ -1,6 +1,6 @@
 
 import java.util.*;
-a
+
 	public class split {
 		public static void main(String[] args) {
 			//It's a method that acts on a string, <StringName>.split(<String sp>);
@@ -17,9 +17,13 @@ a
 
 // Example: "I like apples!".split(" ");
 // it will split at spaces and return an array of ["I","like","apples!"]
+			
 			System.out.println(Arrays.toString("I like apples!".split(" ")));
+			
 // Example 2: "I really like really red apples"split("really")
+			
 			System.out.println(Arrays.toString("I really like really apples!".split("really")));
+			
 // it will split at the word "really" and return an array of ["I "," like ","red apples!"]
 
 //play around with String.split!
@@ -31,7 +35,9 @@ a
 		}
 //Your task Part 1:
 			public static String part1(String sandwich) {
-				return Arrays.toString(sandwich.split("bread"));
+				String[] sandwichArray = new String[3];
+				sandwichArray = sandwich.split("bread");
+				return sandwichArray[1];
 			}
 /*Write a method that take in a string like
 
@@ -49,7 +55,9 @@ a
 
 //Your task pt 2:
 			public static String part2(String sandwich) {
-				return Arrays.toString(sandwich.split(" "));
+				String[] sandwichArray2 = new String[9];
+				sandwichArray2 = sandwich.split(" ");
+				return sandwichArray2[3] + sandwichArray2[4]  + sandwichArray2[5] + sandwichArray2[6] + sandwichArray2[7]; 
 			}
 
 /*Write a method that take in a string like
@@ -57,7 +65,7 @@ a
 * "apples pineapples bread lettus tomato bacon mayo ham bread cheese"
 
 * describing a sandwich
-
+so 3 and 4
 * use String.split to split up the sandwich at the spaces, " ", and return what's in the middle of
 
 * the sandwich and ignores what's on the outside.
