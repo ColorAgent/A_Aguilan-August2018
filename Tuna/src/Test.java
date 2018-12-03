f03a62a2829d39e7d0bd9904652ca89edcc5f0ef
APCS3rd <APCS3rd@MHS-Dreyer-01>
2018-12-03 10:51:31 -0800

    Fix improper frac

--- Tuna/src/Test.java
+++ Tuna/src/Test.java
@@ -3,21 +3,35 @@ import java.util.Arrays;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println(produceAnswer("1/4 + 1_1/2"));
+		System.out.println(produceAnswer("5_3/4 + 1_1/2"));
 		System.out.println(FractionParts("6_5/8"));
 		System.out.println(toImproperFrac(1, 1, 1));
 
 	}
 	public static String produceAnswer(String input) {
+		String[] DefaultValue = {"0","0","1"};
 		String[] Fraction = input.split(" "); //this is a really bad array name
+		
 		String Operand1  = Fraction[0];
+		String[] Fraction1Parts = Operand1.split("[_/]");
+		int whole1 = Integer.parseInt(Fraction1Parts[0]);
+		int denominator1 = Integer.parseInt(Fraction1Parts[1]);
+		int numerator1 = Integer.parseInt(Fraction1Parts[2]);
+		String ImproperFrac1 = toImproperFrac(whole1, denominator1, numerator1);
+		
 		String Operater = Fraction[1];
+		
 		String Operand2  = Fraction[2];
+		String[] Fraction2Parts = Operand2.split("[_/]");
+		int whole2 = Integer.parseInt(Fraction2Parts[0]);
+		int denominator2 = Integer.parseInt(Fraction2Parts[1]);
+		int numerator2 = Integer.parseInt(Fraction2Parts[2]);
+		String ImproperFrac2 = toImproperFrac(whole2, denominator2, numerator2);
 		String Answer= "wow";
 		if (Operater.contains("-")) {
 			
 		}
-		return Answer;
+		return ImproperFrac1;
 	}
 	public static String FractionParts (String Operand) {
 		String[] DefaultValue = {"0","0","1"};
@@ -49,6 +63,7 @@ public class Test {
 		}
 		return PartsDefined;
 	}
+	// turns the mixed number into an improper fraction
 	public static String toImproperFrac(int whole, int numerator, int denominator) {
 		String answer;
 		answer = ((whole*denominator + numerator) + "/" + denominator);
