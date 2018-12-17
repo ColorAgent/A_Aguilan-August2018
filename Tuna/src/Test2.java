7fda8424c7b469cb9dc858800ea81086de920392
APCS3rd <APCS3rd@MHS-Dreyer-01>
2018-12-17 10:30:35 -0800

    C9

---
+++ Tuna/src/Test2.java
@@ -0,0 +1,18 @@
+/* This class test things
+ * @author Alex Aguilan 
+ * Version ¯\_('_')_/¯
+ */
+
+
+public class Test2 {
+
+	public static void main(String[] args) {
+	double x = 2.5, y = 1.99;
+	System.out.println((int)(x/y) + (int)(x*y));
+	}
+	public void process(String s) {
+		s=s.substring(2,3) + s.substring(1,2) + s.substring(0,1);
+	}
+}
+
+
