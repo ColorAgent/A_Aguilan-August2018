1ed552e1f6b634a5d6ec268d1036b889981c6355
Alex Aguilan <Alex_Aguilan@users.noreply.github.com>
2019-03-24 21:50:56 -0700

    fix getgridtext

--- TextExcel/src/textExcel/Spreadsheet.java
+++ TextExcel/src/textExcel/Spreadsheet.java
@@ -6,66 +6,90 @@ import java.util.Arrays;
 
 public class Spreadsheet implements Grid
 {
-	private Cell[][] sheet;
-	private int numOfRows;
-	private int numOfCols;
-	//constructor
-	public Spreadsheet() {
-		//initialize a 20 array of EmptyCells
-		//+1 array for headers and stuff
-		numOfRows = 20;
-		numOfCols = 12;
-		sheet = new Cell [numOfRows][numOfCols];
-		for(int r = 0; r<numOfRows; r++) {
-			for(int c = 0; c<numOfCols; c++) {
-				// rows columns 
-				sheet[r][c] = new EmptyCell();
-			}
-		}
-	}
-	@Override
-	public String processCommand(String command) {
-		String[] commandInspector = command.split(" ", 3);
-		if(commandInspector.length < 2) {
-			return getCell(new SpreadsheetLocation(command)).fullCellText();
-		}
-		if(commandInspector[1].contains("=")) {
-			char col = command.charAt(0);
-			int row = Integer.parseInt(command.substring(1));
-			new SpreadsheetLocation(row, Character.getNumericValue(col));
-			return getGridText();
-		}
-		if(commandInspector[0].contains("clear")) {
-			
-		}
-		return "";
-	}
+    private Cell[][] sheet;
+    private int numOfRows;
+    private int numOfCols;
+    //constructor
+    public Spreadsheet() {
+        //initialize a 20 array of EmptyCells
+        //+1 array for headers and stuff
+        numOfRows = 20;
+        numOfCols = 12;
+        sheet = new Cell [numOfRows][numOfCols];
+        for(int r = 0; r<numOfRows; r++) {
+            for(int c = 0; c<numOfCols; c++) {
+                // rows columns
+                sheet[r][c] = new EmptyCell();
+            }
+        }
+    }
+    @Override
+    public String processCommand(String command) {
 
-	@Override
-	public int getRows()
-	{
-		return numOfRows;
-	}
+        String[] commandInspector = command.split(" ", 3);
+        //cell inspection
+        if(command.length() <= 3) {
+            return getCell(new SpreadsheetLocation(command)).fullCellText();
+        }
+        if(command.contains("=")) {
+            SpreadsheetLocation location = new SpreadsheetLocation(commandInspector[0]);
+            TextCell newCell = new TextCell(commandInspector[2]);
+            sheet[location.getRow()][location.getCol()] = newCell;
+        }
+        // clears specific cell
+        if(command.toLowerCase().contains("clear ")) {
+            SpreadsheetLocation location = new SpreadsheetLocation(commandInspector[1]);
+            sheet[location.getRow()][location.getCol()] = new EmptyCell();
+        } else {
+            if (command.toLowerCase().equals("clear")) {
+                for(int i = 0; i < 20; i++) {
+                    for(int j = 0; j < 12; j++) {
+                        sheet [i][j] = new EmptyCell();
+                    }
+                }
+            }
+        }
+        return getGridText();
+    }
 
-	@Override
-	public int getCols()
-	{
-		// TODO Auto-generated method stub
-		return numOfCols;
-	}
+    @Override
+    public int getRows()
+    {
+        return numOfRows;
+    }
 
-	@Override
-	public Cell getCell(Location loc) {
-		return sheet[loc.getRow()][loc.getCol()];
-	}
+    @Override
+    public int getCols()
+    {
+        // TODO Auto-generated method stub
+        return numOfCols;
+    }
 
-	@Override
-	public String getGridText() {
-		for(int i = 0; i<numOfCols; i++) {
-			char 
-		}
-		
-		return null;
-	}
+    @Override
+    public Cell getCell(Location loc) {
+        return sheet[loc.getRow()][loc.getCol()];
+    }
 
+    @Override
+    public String getGridText(){
+        String grid = "   |";
+        for(int i = 0; i < numOfCols; i++) {
+            grid += (char)(i + 'A') + "         |";
+        }
+         
+        for(int i = 1; i <= numOfRows; i++) {
+            if(i < 10) {
+                grid += "\n" + i + "  |";
+            }else {
+                grid += "\n" + i + " |";
+            }
+            
+            for(int j = 0; j < numOfCols; j++) {
+                grid += sheet[i-1][j].abbreviatedCellText() + "|";
+            }
+        }
+        return grid + "\n";
+    }
 }
+
+
