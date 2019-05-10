58804690a8524cb2615b9a8bc71703e7b05d6d54
APCS3rd <APCS3rd@MHS-Dreyer-01>
2019-05-10 10:55:48 -0700

    Wowee

--- Tuna/src/Test.java
+++ Tuna/src/Test.java
@@ -1,25 +1,7 @@
 import java.util.Arrays;
 
-public class Test {
+public class Test implements Test2 {
 	
-	private static int numOfRows;
-	private static int numOfCols;
-	//constructor
-	public static void main(String[] args) {
-		//initialize a 20 array of EmptyCells
-		numOfRows = 20;
-		numOfCols = 12;
-		// rows columns 
-		String Sheet[][] = new String [numOfRows][numOfCols];
-		for(int i = 0; i<numOfRows; i++)
-		for(int j = 0; j<numOfCols; j++)
-		Sheet[i][j] = "0          |";
-
-		for(int i = 0; i<numOfRows; i++) {
-		    for(int j = 0; j<numOfCols; j++) {
-		        System.out.print(Sheet[i][j]);
-		    }
-		    System.out.println();
-		}		
-	}
 }
+	
+	
