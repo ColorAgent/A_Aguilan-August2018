e690ba5bcfd8070d30b615ebaa06d8f66bb338e1
Alex Aguilan <Alex_Aguilan@users.noreply.github.com>
2019-05-20 23:26:40 -0700

    fix

--- TextExcel/src/textExcel/PercentCell.java
+++ TextExcel/src/textExcel/PercentCell.java
@@ -9,11 +9,8 @@ public class PercentCell extends RealCell{
 	}
 	// text for spreadsheet cell display, must be exactly length 10
 	public String abbreviatedCellText() {
-<<<<<<< HEAD
 		return percent.split("\\.")[0] + ("          ").substring(0, 10);
-=======
 		return percent.split("\\.")[0] + ("%         ").substring(0, 10);
->>>>>>> branch 'master' of https://github.com/ColorAgent/A_Aguilan-August2018.git
 	}
 	// text for individual cell inspection, not truncated or padded
 	public String fullCellText() {
